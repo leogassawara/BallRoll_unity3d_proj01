06c4fbfd2b0e49da49b6819a74d3b24413626721
Leonardo Ogassawara <leogassawara@hotmail.com>
2024-01-19 15:39:40 -0300

    Counting points when the ball collects items and a final message saying "You Win" - End of tutorial

--- Assets/_Game/Scripts/movimentaBola.cs
+++ Assets/_Game/Scripts/movimentaBola.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Windows;
+using UnityEngine.UI;
+using TMPro;
 
 public class movimentaBola : MonoBehaviour
 {
@@ -10,9 +12,17 @@ public class movimentaBola : MonoBehaviour
     public float velocidade;
 
     public GameObject particulaItem;
+
+    public TMP_Text textoPontos;
+    public TMP_Text textoFinal;
+
+    private int pontos;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        textoFinal.text = "";
+
+        textoPontos.text = textoPontos.text + pontos.ToString();
     }
 
     void FixedUpdate()
@@ -22,12 +32,24 @@ public class movimentaBola : MonoBehaviour
      
     }
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("item"))
         {
             Instantiate(particulaItem, other.gameObject.transform.position, Quaternion.identity);
             Destroy(other.gameObject);
+            MarcaPonto();
+  
+        }
+    }
+
+    void MarcaPonto()
+    {
+        pontos++;
+        textoPontos.text = "Score: " + pontos.ToString();
+        if (pontos == 4)
+        {
+            textoFinal.text = "You Win!!!";
         }
     }
 }
